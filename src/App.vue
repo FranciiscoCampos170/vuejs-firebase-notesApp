a9a7a729a695068a58d19ad7fd9a4700c5db41ae
Francisco Campos <franciiscocampos170@gmail.com>
2020-03-10 22:25:47 +0100

    create new style

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,16 @@
   <div id="app">
     
     <div class="row justify-content-center my-5" >
-      <div class="notes">
-          <div class="notes-header">
-            Create new note
-               <button class="float-right btn-sm" @click="addNote"> <i class="fa fa-check" aria-hidden="true"></i></button>
-          </div>
-          <textarea name="new-note" v-model="newNote" class="newNote"></textarea>
-        </div>
+      <div id="myDIV" class="header">
+          <input type="text" id="myInput" placeholder="Note..." v-model="newNote">
+          <span @click="addNote" class="addBtn">Add</span>
+       </div>
     </div>
 
   <div class="row justify-content-center my-5">
-      <ul class="list-group">
-        <li class="list-group-item" v-for="notes in notes" :key="notes.id">
-        <div class="notes">
-          <div class="notes-header">
-            {{notes.timestamp}}
-            <button class="float-right btn-sm" @click="deleteItem(notes.id)"> <i class="fa fa-trash" aria-hidden="true"></i></button>
-            <button class="float-right btn-sm btnsimple" @click=" updateItem(notes.id, notes.content)"> <i class="fa fa-refresh" aria-hidden="true"></i></button>
-          </div>
-          <textarea name="note" id="" cols="30" rows="10" v-model="notes.content"></textarea>
-        </div>
+      <ul id="myUL">
+        <li v-for="notes in notes" :key="notes.id">
+          <span class="text-muted time">({{notes.timestamp}})</span> - {{ notes.content }}
         </li>
       </ul>
     </div>
@@ -67,7 +57,7 @@ export default {
           console.log(err);
         })
         console.log("Note Updated");
-        
+
     }
   },
   created(){
@@ -92,32 +82,119 @@ export default {
   #app{
     
   }
-  
-  ul {
-     list-style-type: none;
+  .time{
+    font-size: 0.8em;
   }
-  .notes {
-    width: 560px;
-    background: #eee;
-    box-shadow: #999 1px 1px 3px;
-    margin: 30px 0;
-    height: auto;
-    display: block;
-    overflow: hidden;
-    position: relative;
-  }
-  .notes-header{
-    line-height: 1.15;
-    padding: 10px 10px;
-    font-family: sans-serif;
-    font-size: 0.7em;
-    font-style: italic;
-    color: #000;
-  }
-  textarea{
-    width: 100%;
-  }
-  .btnsimple{
-    : 130px;
-  }
+* {
+  box-sizing: border-box;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+}
+
+/* Style the list items */
+ul li {
+  cursor: pointer;
+  position: relative;
+  padding: 12px 8px 12px 40px;
+  background: #eee;
+  font-size: 18px;
+  transition: 0.2s;
+
+  /* make the list items unselectable */
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+/* Set all odd list items to a different color (zebra-stripes) */
+ul li:nth-child(odd) {
+  background: #f9f9f9;
+}
+
+/* Darker background-color on hover */
+ul li:hover {
+  background: #ddd;
+}
+
+/* When clicked on, add a background color and strike out text */
+ul li.checked {
+  background: #888;
+  color: #fff;
+  text-decoration: line-through;
+}
+
+/* Add a "checked" mark when clicked on */
+ul li.checked::before {
+  content: '';
+  position: absolute;
+  border-color: #fff;
+  border-style: solid;
+  border-width: 0 2px 2px 0;
+  top: 10px;
+  left: 16px;
+  transform: rotate(45deg);
+  height: 15px;
+  width: 7px;
+}
+
+/* Style the close button */
+.close {
+  position: absolute;
+  right: 0;
+  top: 0;
+  padding: 12px 16px 12px 16px;
+}
+
+.close:hover {
+  background-color: #f44336;
+  color: white;
+}
+
+/* Style the header */
+.header {
+  background-color: #f44336;
+  padding: 30px 40px;
+  color: white;
+  text-align: center;
+}
+
+/* Clear floats after the header */
+.header:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Style the input */
+input {
+  margin: 0;
+  border: none;
+  border-radius: 0;
+  width: 75%;
+  padding: 10px;
+  float: left;
+  font-size: 16px;
+}
+
+/* Style the "Add" button */
+.addBtn {
+  padding: 10px;
+  width: 25%;
+  background: #d9d9d9;
+  color: #555;
+  float: left;
+  text-align: center;
+  font-size: 16px;
+  cursor: pointer;
+  transition: 0.3s;
+  border-radius: 0;
+}
+
+.addBtn:hover {
+  background-color: #bbb;
+}
 </style>
